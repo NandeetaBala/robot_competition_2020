375f42310f84dbd93a6deca925ee8bdd2e0f8395
NandeetaBala <nbala@vassar.edu>
2020-05-06 13:20:19 -0400

    Milestone 3

---
+++ Milestone 3/milestone_3_code/milestone_3_code.ino
@@ -0,0 +1,21 @@
+#include <ZumoShield.h>
+
+ZumoMotors motors;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  motors.setLeftSpeed(100);
+  motors.setRightSpeed(100);
+  delay(4000);
+  motors.setLeftSpeed(0);
+  motors.setRightSpeed(0);
+  while(true) {
+
+}
+
+}
