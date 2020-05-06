83b7bda66f87e2400ab334977e2372d27492a8ac
NandeetaBala <nbala@vassar.edu>
2020-05-06 14:01:46 -0400

    milestone 2 completed!!

--- Milestone 2/milestone_2_code/milestone_2_code.ino
+++ Milestone 2/milestone_2_code/milestone_2_code.ino
@@ -1,21 +1,37 @@
+#include <Wire.h>
 #include <ZumoShield.h>
 
 ZumoMotors motors;
+ZumoReflectanceSensorArray linesensors(QTR_NO_EMITTER_PIN);
+ZumoBuzzer buzzer;
 
-void setup() {
-  // put your setup code here, to run once:
-
+void setup() {//this is using the read line method
+  int i;
+  int spin_direction = 1;
+  motors.setSpeeds(80*spin_direction, -80*spin_direction);
+  for(i = 0; i<150; i++){
+    linesensors.calibrate();
+    if(i%50 == 25){ // every 50 loops, starting on loop 25...
+      spin_direction = -spin_direction;
+      motors.setSpeeds(80*spin_direction, -80*spin_direction);
+    }
+    delay(20);
+  }
+  motors.setSpeeds(0,0);
+  delay(500);
 }
 
+unsigned int sensor_vals[6];//this is proportional derivative control
+int BASE_SPEED = 200;
+double PROPORTION_GAIN = 0.2;
+double DERIVATIVE_GAIN = 3;//optimal value for smoothness
+int last_error = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-  motors.setLeftSpeed(100);
-  motors.setRightSpeed(100);
-  delay(4000);
-  motors.setLeftSpeed(0);
-  motors.setRightSpeed(0);
-  while(true) {
-
-}
-
+  int line_position = linesensors.readLine(sensor_vals);
+  int error = line_position - 2500;
+  int error_change = error - last_error;
+  int left_speed = BASE_SPEED + PROPORTION_GAIN * error + DERIVATIVE_GAIN * error_change;
+  int right_speed = BASE_SPEED + -PROPORTION_GAIN * error + -DERIVATIVE_GAIN * error_change;
+  last_error = error;
+  motors.setSpeeds(left_speed, right_speed);
 }
